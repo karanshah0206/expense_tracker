24443eb6d4fa747c4a4a86974a475966e681efd4
Karan Shah <karanshah0206@outlook.com>
2025-04-08 15:23:35 +0530

    Add formatted date getter and category icons in expense model

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final DateFormat formatter = DateFormat.yMd();
 const Uuid uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.dinner_dining,
+  Category.travel: Icons.directions_car,
+  Category.leisure: Icons.hiking,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -17,4 +27,6 @@ class Expense {
     required this.dateTime,
     required this.category,
   }) : id = uuid.v4();
+
+  String get formattedDate => formatter.format(dateTime);
 }
