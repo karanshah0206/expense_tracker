d81912b7505e5c0643384c70878b759e92d9813f
Karan Shah <karanshah0206@outlook.com>
2025-04-10 02:47:22 +0530

    Created widget to show bar chart of expenses by category

---
+++ lib/widgets/category_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final double barSize, ceilingSize;
+  final Color color;
+
+  const ChartBar({
+    required this.barSize,
+    required this.ceilingSize,
+    required this.color,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: FractionallySizedBox(
+          heightFactor: barSize / ceilingSize,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(8),
+              ),
+              color: color,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_bar_chart.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widgets/category_bar.dart';
+
+class Chart extends StatelessWidget {
+  final Map<Category, double> _buckets;
+  final ColorScheme colorScheme;
+
+  Chart({super.key, required List<Expense> expenses, required this.colorScheme})
+    : _buckets = {
+        for (Category category in Category.values)
+          category: expenses
+              .where((expense) => expense.category == category)
+              .fold(0.0, (sum, expense) => sum + expense.amount),
+      };
+
+  double get _maxNetExpense {
+    double result = 0;
+    for (double netExpense in _buckets.values) {
+      if (netExpense > result) {
+        result = netExpense;
+      }
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+          colors: [
+            colorScheme.inversePrimary.withAlpha(200),
+            colorScheme.inversePrimary.withAlpha(0),
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (Category category in Category.values)
+                  Expanded(
+                    child: ChartBar(
+                      barSize: _buckets[category]!,
+                      ceilingSize: _maxNetExpense,
+                      color: colorScheme.primary,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children:
+                _buckets.keys
+                    .map(
+                      (Category category) => Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4),
+                          child: Icon(
+                            categoryIcons[category],
+                            color: colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
